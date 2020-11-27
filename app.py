c16ab90ce60d7605164386cc415d191e84836598
plparadis <pierre_lucparadis@hotmail.com>
2020-11-27 11:20:15 -0500

    add logo and title

--- app.py
+++ app.py
@@ -10,7 +10,14 @@ plt.rcParams['xtick.color'] = TextColor
 plt.rcParams['ytick.color'] = TextColor
 fontname = 'Helvetica LT Std'
 
-st.set_page_config(page_title="",
+st.set_page_config(page_title="Thermal Storage Tank",
                     page_icon="static/favicon.ico",
                     layout="centered",
                     initial_sidebar_state="auto")
+
+st.sidebar.image("static/logo.png")
+
+st.title("Thermal Storage Tank")
+st.markdown("a simple simulation tool to simulate stratified thermal storage tank")
+
+
