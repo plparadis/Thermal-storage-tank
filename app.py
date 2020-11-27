1fc39cab21493b82f5fb207277ee676c951e1b26
plparadis <pierre_lucparadis@hotmail.com>
2020-11-27 11:15:10 -0500

    initial test with only favicon and named app

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 # Author PLParadis
 import streamlit as st
+from matplotlib import pyplot as plt
+
 
 TextColor = "#404041"
 plt.rcParams['text.color'] = TextColor
@@ -8,7 +10,7 @@ plt.rcParams['xtick.color'] = TextColor
 plt.rcParams['ytick.color'] = TextColor
 fontname = 'Helvetica LT Std'
 
-st.beta_set_page_config(page_title="",
-                        page_icon="static/favicon.ico",
-                        layout="centered",
-                        initial_sidebar_state="auto")
+st.set_page_config(page_title="",
+                    page_icon="static/favicon.ico",
+                    layout="centered",
+                    initial_sidebar_state="auto")
